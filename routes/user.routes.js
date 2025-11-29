7e9cdcbe697222affdd0f9549e39b2fa73c839b9
Abhishek Wake <abhishekwake111@gmail.com>
2025-11-30 00:01:08 +0530

    implement organised route structure with auth,user,subscriptions

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -7,3 +7,5 @@ userRouter.get('/:id', (req,res)=> { res.send( {title: "GET user details"} )});
 userRouter.post('/', (req,res)=> { res.send( {title: "Create new user"} )});
 
 userRouter.delete('/:id', (req,res)=> { res.send( {title: "delete user"} )});
+
+export default userRouter;
