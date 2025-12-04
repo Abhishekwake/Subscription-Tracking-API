53137cd0cef2de19a203b711cfb8ce611baa18b7
Abhishek Wake <abhishekwake111@gmail.com>
2025-12-04 19:51:58 +0530

    add authorize middleware to validate JWT,check user existence and attach user to the request object

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
+import { getUser,getUsers } from "../controllers/user.controller.js";
+import { authorize } from "../middlewares/auth.middleware.js";
 const userRouter = Router();
-userRouter.get('/', (req,res)=> { res.send( {title: "GET All Users"} ); });
+// userRouter.get('/', (req,res)=> { res.send( {title: "GET All Users"} ); }); older
+userRouter.get('/', authorize,getUsers);
 
-userRouter.get('/:id', (req,res)=> { res.send( {title: "GET user details"} )});
+// userRouter.get('/:id', (req,res)=> { res.send( {title: "GET user details"} )}); older
+userRouter.get('/:id',getUser);
 
 userRouter.post('/', (req,res)=> { res.send( {title: "Create new user"} )});
 
